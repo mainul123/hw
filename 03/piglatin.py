e58be127987450977e65c6f08bd31f7ca24b2608
Mainul Nishan <you@example.com>
2017-09-17 22:55:03 -0400

    git push

---
+++ 03/piglatin.py
@@ -0,0 +1,30 @@
+print('welcome to Mainuls piglatin program')
+
+pyg ="ay"
+
+original = input('Enter a Word:')
+
+if len(original) > 0 and original.isalpha():
+    print(original)
+    word = original.lower()
+    first = original[0]
+else:
+    print ("empty")
+
+new_word = word + first +pyg
+new_word = new_word[1:]
+print (new_word)
+
+
+'''
+notes for me**
+
+first new_word line doesnt remove the first letter
+
+.isalpha= must be a letter
+
+second new word runs it from the second word and
+ends at the last word 
+
+'''
+
