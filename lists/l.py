473671525ed03acf7f0665e4520515d55f03fb26
Mike Zamansky <zamansky@gmail.com>
2017-09-19 10:24:20 -0400

    added r.py

--- lists/l.py
+++ lists/l.py
@@ -1,3 +1,7 @@
+import random
+
+
+
 l=[]
 l2=['a','b','c','d','e','f','g']
 
@@ -25,3 +29,15 @@ l.append(l2)
 l3=['a','b','c']
 l4= [1,2,3,4,5]
 longlist = l3+l4
+
+print(random.randrange(5,50))
+
+for i in range(10):
+    x = random.randrange(5,50)
+    print(x)
+    
+    
+l=[1,2,3,4,5]
+print(l)
+random.shuffle(l)
+print(l)
