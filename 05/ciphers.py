8c2ab9090eabb36379ecbbba91e137b2d16e7ec6
Mainul Nishan <you@example.com>
2017-09-28 00:59:33 -0400

    git push

---
+++ 05/ciphers.py
@@ -0,0 +1,32 @@
+
+
+#letter shift
+for letter in input():
+    ascii_val = ord(letter)
+    plus = ascii_val + 1
+    newchar = chr(plus)
+    print(letter, ":", ascii_val, " rotated:", newchar, ":", plus)
+    
+    
+    
+    #string shift
+message = "just a simple test!"
+key = 11
+coded_message = ""
+for ch in message:
+    code_val  = ord(ch) + key
+    if ch.isalpha():
+        if code_val > ord('z'):          
+            coded_message = coded_message + chr(code_val)
+    else:
+        coded_message = coded_message + ch
+print (message)
+print (coded_message)
+
+    #all possible combos for string
+
+from itertools import permutations
+perms = [''.join(p) for p in permutations('stack')]
+perms
+
+
