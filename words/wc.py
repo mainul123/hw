2edccf03dd076e3aa118f656a32f29d01e1d8fc9
Mike Zamansky <zamansky@gmail.com>
2017-11-09 10:20:59 -0500

    Added text generator

--- words/wc.py
+++ words/wc.py
@@ -1,3 +1,5 @@
+import random
+
 def remove_non_alpha(w):
     """
     input: w - string representing a "word"
@@ -16,6 +18,18 @@ def bwcd(wordlist):
         d[w] = d[w] + 1
     return d
 
+def build_word_chain_dict(wordlist):
+    d={}
+    for i in range(1,len(wordlist)):
+        w1 = wordlist[i-1]
+        w2 = wordlist[i]
+        d.setdefault(w1,[])
+        d[w1].append(w2)
+        
+    return d
+
+
+
 def bwcff(f):
     """
     input: f - string representing a filename
@@ -28,8 +42,20 @@ def bwcff(f):
         w = w.lower()
         w = remove_non_alpha(w)
         l.append(w)
-    d = bwcd(l)
+    d = build_word_chain_dict(l)
     return d
 
+def generate_text(d,start_word,length=50):
+    wordlist = []
+    next = start_word
+    for i in range(length):
+        if next not in d:
+            break
+        wordlist.append(next)
+        next = random.choice(d[next])
+    return " ".join(wordlist)
 
-d = bwcff("hamlet.txt")
+
+hamlet = bwcff("hamlet.txt")
+psalms = bwcff("psalms.txt")
+sonnets = bwcff("sonnets.txt")
