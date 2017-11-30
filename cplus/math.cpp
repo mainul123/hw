586834078ecbecde0b8ea60891d24f19a4f09fab
Mainul Nishan <you@example.com>
2017-11-30 10:21:54 -0500

    "added c++"

---
+++ cplus/math.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+
+
+int main() {
+  int i;
+  double d1,d2;
+  d1 = 123.45
+
+  i = 7;
+  d2 = i;
+  std::cout << "Hello" << '\n';
+  std::cout << "i = " << i << std::endl;
+  std::cout << i/2 << '\n';
+  std::cout << d2 << "  " << d2/2 << std::endl;
+  i = d2/2;
+  std::cout << i << '\n';
+  std::cout << d1 << std::endl;
+  std::cout << std::setprecision(2) << d1  << std::endl;
+  std::cout << c << '\n';
+  return 0;
+}
